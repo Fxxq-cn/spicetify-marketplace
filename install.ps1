9b153982c8613a6147b9aada0c69b0da383ee0c6
Fxxq-cn <LewiStruc@outlook.com>
2026-01-18 16:34:41 -0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -22,7 +22,7 @@ Invoke-WebRequest -Uri $spotxUrl -OutFile $spotxLocalPath -UseBasicParsing
     "if (`$test_js) {`n$body`n}"
 } | Set-Content -Path $spotxLocalPath -Encoding UTF8
 
-& $spotxLocalPath -confirm_uninstall_ms_spoti -sp-over -new_theme -block_update_on -podcasts_on -dev -exp_spotify -adsections_off -cl 20000 -topsearchbar -newFullscreenMode -canvasHome -rightsidebarcolor -hide_col_icon_off
+& $spotxLocalPath -confirm_uninstall_ms_spoti -new_theme -block_update_on -podcasts_on -dev -exp_spotify -adsections_off -cl 20000 -topsearchbar -newFullscreenMode -canvasHome -rightsidebarcolor -hide_col_icon_off
 
 Write-Output "Installing Spicetify..."
 iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex
